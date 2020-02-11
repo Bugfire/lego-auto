342b323bd970f4b60d30d48fcdf2031ab20ba2ea
bugfire <bugfire@users.noreply.github.com>
2020-02-11 17:34:21 +0900

    Hmm, my qnap dns is broken?

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -7,10 +7,10 @@ CERTS=/data/certs
 while :
 do
     if [ -d "$CERTS" ]; then
-        ./lego --accept-tos --path="$CERTS" --email="$EMAIL" --dns="$DNS" --domains="$DOMAINS" renew --days 30
+        ./lego --accept-tos --path="$CERTS" --email="$EMAIL" --dns="$DNS" --domains="$DOMAINS"  --dns.resolvers 8.8.8.8 renew --days 30
     else
         mkdir "$CERTS"
-        ./lego --accept-tos --path="$CERTS" --email="$EMAIL" --dns="$DNS" --domains="$DOMAINS" run
+        ./lego --accept-tos --path="$CERTS" --email="$EMAIL" --dns="$DNS" --domains="$DOMAINS"  --dns.resolvers 8.8.8.8 run
     fi
     # 1day = 24*3600 = 86400
     sleep 86400
